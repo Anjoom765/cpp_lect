11998e10702dc7ba070b4f1a125f916065eebd44
anjoom <anjoomyas@gmail.com>
2022-02-25 01:33:34 +0530

    c13

---
+++ sorting/quicksort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int partition(int arr[],int low,int high)
+{
+   int pivot= arr[high];
+   int i=low-1;
+   for (int j = low; j < high-1; j++)
+   {
+       if(arr[j]<=arr[pivot])
+       {
+           i++;
+           swap(arr[j],arr[i]);
+
+       }
+       
+   }
+   swap(arr[i+1],arr[high]);
+   return (i+1);
+
+   
+}
+void quickSort(int arr[],int low, int high)
+{
+ if(low<high)
+ {
+    int pi=partition(arr,low,high);
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+ }
+}
+
+int main()
+{
+ 
+ int arr[]
+}
