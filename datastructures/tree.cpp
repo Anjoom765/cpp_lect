ff248a15a3b1607c92c0b443bb5ddff5c7f36a06
anjoom <anjoomyas@gmail.com>
2022-02-25 23:28:53 +0530

    14

---
+++ datastructures/tree.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+struct Node{
+  int data;
+  struct Node* right;
+  struct Node* left;
+  Node(int val)
+  {
+     data=val;
+     right=NULL;
+     left=NULL;
+  }
+
+};
+int main()
+{
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(4);
+    root->left->left=new Node(5);
+    root->left->right=new Node(10);
+
+}
--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int partition(int arr[],int low,int high)
 {
    int pivot= arr[high];
-   int i=low-1;
+   int i=(low-1);
    for (int j = low; j < high-1; j++)
    {
-       if(arr[j]<=arr[pivot])
+       if(arr[j]<pivot)
        {
            i++;
-           swap(arr[j],arr[i]);
+           swap(arr[i],arr[j]);
 
        }
        
@@ -25,13 +25,25 @@ void quickSort(int arr[],int low, int high)
  if(low<high)
  {
     int pi=partition(arr,low,high);
-    quickSort(arr,low,pi-1);
-    quickSort(arr,pi+1,high);
+    quickSort(arr,low,(pi-1));
+    quickSort(arr,(pi+1),high);
  }
 }
 
 int main()
 {
  
- int arr[]
+ int size=0;
+ cin>>size;
+ int arr[size];
+ for (int i = 0; i < size; i++)
+ {
+     cin>>arr[i];
+ }
+ cout<<"after sorting"<<endl;
+ quickSort(arr,0,size-1);
+  for (int i = 0; i < size; i++)
+ {
+     cout<<arr[i]<<" ";
+ }
 }
